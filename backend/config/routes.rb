4cdf0646ab7e60f5c88abdad7c165b2842826965
Oneil <oneilswilliams@gmail.com>
2019-12-03 18:40:36 -0500

    stuffy

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :orders
   resources :shoes
   resources :users
+  resources :login, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
