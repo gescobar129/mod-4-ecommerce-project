9fe4cd4f691170e251cff4bc3d5592916fb9f18a
Oneil <oneilswilliams@gmail.com>
2019-12-04 11:21:31 -0500

    my

--- backend/app/controllers/login_controller.rb
+++ backend/app/controllers/login_controller.rb
@@ -1,20 +1,7 @@
 class LoginController < ApplicationController
-    # def create
-    #     current_user = User.find_by(username: params[:session][:username])
-    #     if current_user && current_user.authenticate(params[:session][:password])
-    #         session[:user_id] = @current_user.id
-    #     else
-    #         render json: {error: "You done fucked up"}
-    #     end 
-    # end 
-
-    # def destroy
-    #     session.delete(:user_id)
-    #     redirect_to login_path
-    # end
 
     def create
-        user = User.find_by("lower(username) = ?", params[:username].downcase)
+        user = User.find_by(username: params[:username].downcase)
         if user && user.authenticate(params[:password])
           render json: { token: token(user.id), user_id: user.id }
         else 
--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all
+        render json: users
+    end 
+
+    def show 
+        user =User.find.find(params[:id])
+        render json: user
+    end 
+
     def create
         user = User.create(user_params)
         
